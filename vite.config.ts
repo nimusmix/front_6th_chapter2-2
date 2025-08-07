22c19ff931b7e8a9aaac6609d4f965f9aa7aa0e4
nimusmix <nimusmix@gmail.com>
2025-08-08 02:24:56 +0900

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
-import { defineConfig as defineTestConfig, mergeConfig } from 'vitest/config';
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
+import { defineConfig as defineTestConfig, mergeConfig } from 'vitest/config';
 
 export default mergeConfig(
   defineConfig({
+    base: '/front_6th_chapter2-2/',
     plugins: [react()],
     build: {
       rollupOptions: {
@@ -19,7 +20,7 @@ export default mergeConfig(
     test: {
       globals: true,
       environment: 'jsdom',
-      setupFiles: './src/setupTests.ts'
+      setupFiles: './src/setupTests.ts',
     },
   })
-)
+);
