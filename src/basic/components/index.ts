8a752264ca66033408b5a92b66ecfff2b0cc51c4
nimusmix <nimusmix@gmail.com>
2025-08-08 01:29:39 +0900

    feat: 상품 관리 및 쿠폰 관리 컴포넌트 분리

--- src/basic/components/index.ts
+++ src/basic/components/index.ts
@@ -4,3 +4,5 @@ export { ProductList } from './ProductList';
 export { Cart } from './Cart';
 export { NotificationList } from './NotificationList';
 export { AdminPanel } from './AdminPanel';
+export { ProductManagement } from './ProductManagement';
+export { CouponManagement } from './CouponManagement';
